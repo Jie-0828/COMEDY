00b566b8bd1f26565f4c897a937e33b806617b1d
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2023-10-09 10:14:08 +0800

    Update decayer.py

--- caddy/decayer.py
+++ caddy/decayer.py
@@ -6,13 +6,9 @@ class Decayer(nn.Module):
     def __init__(self, w, decay_method='exp'):
         super(Decayer,self).__init__()
         self.decay_method = decay_method
-        # self.linear = nn.Linear(1,1)
         self.w = w
-        # self.w =nn.Parameter(torch.Tensor(1)).unsqueeze(dim=0)
-        # nn.init.xavier_uniform_(self.w)
 
     def exponetial_decay(self, delta_t):
-        # return self.linear(torch.exp(-self.w*delta_t).unsqueeze(dim=0))
         return torch.exp(-self.w*delta_t)
     def log_decay(self, delta_t):
         return 1/torch.log(2.7183 + self.w*delta_t)
