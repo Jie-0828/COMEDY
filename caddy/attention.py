5440d9c87e911c3e3ac880b87f9bc9cc571030d9
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2024-01-11 20:22:32 +0800

    Update attention.py

--- caddy/attention.py
+++ caddy/attention.py
@@ -1,20 +1,27 @@
+import math
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from decayer import Decayer
+from time2vec import Time2Vec
 
 
 class Attention(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels,time_dim):
         super(Attention, self).__init__()
-        self.in_channels = in_channels
+        self.in_channels = in_channels  #
         self.out_channels = out_channels
+        # self.time2vec = Time2Vec('sin', time_dim)
 
         self.weight_w = nn.Parameter(torch.FloatTensor(self.in_channels, self.out_channels))
         self.weight_a = nn.Parameter(torch.FloatTensor(self.out_channels*2, 1))
 
         # self.leakyrelu = nn.LeakyReLU()
+
+        # self.decayer = Decayer(w, decay_method)
 
         self.init_parameters()
 
@@ -23,12 +30,14 @@ class Attention(nn.Module):
         nn.init.xavier_uniform_(self.weight_a)
 
     def forward(self, x_i,x_j):
-        # 1.Map the node space
         wh1 = torch.mm(x_i.unsqueeze(dim=0), self.weight_w)
         wh2 = torch.mm(x_j.unsqueeze(dim=0), self.weight_w)
         x_cat = torch.cat([wh1, wh2], dim=1)
 
-        # 2.Calculating attention scores
         e = torch.mm(x_cat, self.weight_a)
 
+
         return e
+
+
+
