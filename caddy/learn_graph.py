ccd03aa57333af5555c21c666be8d4ec414aa11c
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2024-01-11 20:27:35 +0800

    Update learn_graph.py

--- caddy/learn_graph.py
+++ caddy/learn_graph.py
@@ -9,7 +9,7 @@ from model import *
 from tqdm import tqdm, trange
 
 
-def evaluate(test_edge,test_labels,model_, classification, edge_agg,dict_gc_test):
+def evaluate(test_edge,test_labels,caddy, classification, edge_agg,dict_gc_test):
     """
     Test the performance of the model
     Parameters:
@@ -25,7 +25,7 @@ def evaluate(test_edge,test_labels,model_, classification, edge_agg,dict_gc_test
         for index in pbar:
             edge = test_edge[index]
             dict_gc = dict_gc_test[index]
-            embeddings = model_(edge,test_labels[index],dict_gc)
+            embeddings = caddy(edge,test_labels[index],dict_gc)
             edge_embed = edge_agg_function(edge_agg, embeddings).view(1, -1)
 
             logists_edge = torch.softmax(classification(edge_embed),1)
@@ -39,7 +39,7 @@ def evaluate(test_edge,test_labels,model_, classification, edge_agg,dict_gc_test
 
     return predicts_socre_all,predicts_test_all
 
-def train_model(train_labels,train_edge,model_,optimizer,classification,device,edge_agg,dict_gc_all,models):
+def train_model(train_labels,train_edge,caddy,optimizer,classification,device,edge_agg,dict_gc_all,models):
 
     criterion=nn.CrossEntropyLoss()
 
@@ -53,7 +53,7 @@ def train_model(train_labels,train_edge,model_,optimizer,classification,device,e
     for index in pbar:
         edge=train_edge[index]
         dict_gc=dict_gc_all[index]
-        embeddings=model_(edge,train_labels[index],dict_gc)
+        embeddings=caddy(edge,train_labels[index],dict_gc)
         edge_embed=edge_agg_function(edge_agg,embeddings).view(1,-1)
 
         logists_edge = classification(edge_embed)
@@ -70,7 +70,7 @@ def train_model(train_labels,train_edge,model_,optimizer,classification,device,e
         nn.utils.clip_grad_norm_(model.parameters(), 5)
     optimizer.step()
 
-    return loss_all, model_,classification
+    return loss_all, caddy,classification
 
 
 
