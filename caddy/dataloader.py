bde792b8a74b6c9554ae94dbbb2a63dac4c89908
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2024-01-11 20:23:00 +0800

    Update dataloader.py

--- caddy/dataloader.py
+++ caddy/dataloader.py
@@ -1,63 +1,28 @@
-import random
-import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
-def load_data_node(path,dataset):  # file_path,dataset_name
-    edge_order = []
-    dict_edge={}
-    set_node=set()
-    dict_edge_new={}
-    dict_node={}
 
-    #read dataset file
-    with open(path,'r',encoding='gbk', errors='ignore')as fd:
-        if dataset in ['UCI', 'DIGG']:
-            fd.readline()
-        for line in fd:
-            if line=='\n':
-                break
-            if dataset in ['UCI', 'DIGG']:
-                line = line.strip('\n').split(' ')
-                timestampe = float(line[3])
-            elif dataset in ['ia-contacts_dublin','fb-forum']:
-                line = line.strip('\n').split(',')
-                timestampe = float(line[2])
-            else:
-                line = line.strip('\n').split(',')
-                timestampe = float(line[3])
-            user_id = int(line[0])
-            item_id = int(line[1])
-            set_node.add(user_id)
-            set_node.add(item_id)
-            
-            #Remove self-loops and repeated edges
-            if user_id!=item_id:
-                if (user_id, item_id) not in dict_edge: 
-                    dict_edge[(user_id, item_id)] = [timestampe]
-                else:
-                    dict_edge[(user_id, item_id)].append(timestampe)
+class Classification(nn.Module):
+    """One-layer classification model
+    Parameters:
+        input_size: Input dimension
+        num_classes: Number of categories
+    return:
+        logists: The maximum probability corresponds to the label
+    """
+    def __init__(self, input_size, num_classes):
+        super(Classification, self).__init__()
+        self.fc1 = nn.Linear(input_size, num_classes)
+        # self.fc2 = nn.Linear(input_size, num_classes)
+        self.init_params()
 
-    #Node reindex
-    np_node=np.array(sorted(list(set_node)))
-    np_node=np.unique(np_node, return_index=True, axis=0)
-    for i in range(len(np_node[0])):
-        dict_node[np_node[0][i]]=np_node[1][i]
+    def init_params(self):
+        for param in self.parameters():
+            if len(param.size()) == 2:
+                nn.init.xavier_uniform_(param)
 
+    def forward(self, x):
+        logists=self.fc1(x)
+        return logists
 
-    #Give each edge a timestamp
-    for key,value in dict_edge.items():
-        new_time=np.random.choice(value, size=1)
-        if new_time[0] not in dict_edge_new:
-            dict_edge_new[new_time[0]]=[[dict_node[key[0]],dict_node[key[1]],new_time[0]]]
-        else:
-            dict_edge_new[new_time[0]].append([dict_node[key[0]],dict_node[key[1]],new_time[0]])
-
-
-    ori_time=sorted(dict_edge_new.keys())[0]
-    for key in sorted(dict_edge_new.keys()):
-        random.shuffle(dict_edge_new[key])# The edges under the same timestamp are randomly shuffled
-        for i in dict_edge_new[key]:
-            edge_order.append([i[0],i[1],int(i[2]-ori_time)])
-
-    return edge_order,len(dict_node)
