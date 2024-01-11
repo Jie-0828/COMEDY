4d425a197e421145f7a332cce2644a9c06c6383f
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2024-01-11 20:24:38 +0800

    Update model.py

--- caddy/model.py
+++ caddy/model.py
@@ -1,6 +1,9 @@
+import copy
+
 import networkx as nx
 import torch
 import torch.nn as nn
+from scipy.sparse import lil_matrix
 from sklearn.model_selection import train_test_split
 from torch.nn import init
 from torch.autograd import Variable
@@ -9,98 +12,122 @@ import numpy as np
 import time
 import random
 from sklearn.metrics import f1_score
-from collections import defaultdict
+from collections import defaultdict, deque
 from sklearn import metrics
 import torch.nn.functional as F
 from tqdm import tqdm
 
 from decayer import Decayer
 from attention import Attention
-from updata_adj import gravity_centrality
+from get_gc import get_in_neighbors
+from time2vec import Time2Vec
 
 
 class CADDY(nn.Module):
-    def __init__(self, num_nodes, embedding_dims, hidden_size, dropout,alpha, device, gcn=False):
-        super(CADDY, self).__init__()
+    def __init__(self, num_nodes, embedding_dims, hidden_size, dropout, device,threshold,window_size, gcn=False):
+        super(Model, self).__init__()
         self.decayer=Decayer(2)
+        self.decayer_feature = Decayer(2,"rev")
         self.num_nodes = num_nodes
         self.embedding_dims = embedding_dims
-        self.attention = Attention(self.embedding_dims, self.embedding_dims).to(device)
+        self.attention = Attention(self.embedding_dims, self.embedding_dims,embedding_dims).to(device)
         self.gcn = gcn
         self.device = device
-        self.threshold = 50000#100000
-        self.alpha=alpha
-
-        self.linear_time = nn.Linear(1, self.embedding_dims).to(device)
-        self.linear_node = nn.Linear(1, self.embedding_dims).to(device)
+        self.threshold =threshold
+        self.window_size = window_size  # The length of the feature queue
+        self.linear_time = nn.Linear(1, embedding_dims).to(device)
+        # self.time_encode = TimeEncode(embedding_dims)
+        self.linear_node = nn.Linear(1, embedding_dims).to(device)
         self.weight = nn.Parameter(torch.FloatTensor(hidden_size, self.embedding_dims*2)).to(device)
         nn.init.xavier_uniform_(self.weight)
+        self.edge=0#Record the current interactive edge
+        self.feat=0#Record the current node features
+        self.graph_remain=0
 
     def initialize(self):
-        # Initialize an empty graph
-        self.graph = nx.Graph()
-        self.adj_time = torch.zeros([self.num_nodes]).to(self.device)  # Saves the time of the last interaction
-        self.adj = torch.zeros([self.num_nodes, self.num_nodes]).to(self.device)
-
-    def forward(self, edge,label):
-        # Update network structure
-        if label==0:
-            self.adj[edge[1]][edge[0]]+=1
-            self.graph.add_edge(edge[0], edge[1])
+        self.feature_bank = {}  # historical information bank
+        self.adj_time = torch.zeros([self.num_nodes]).to(self.device)  # record The time of the last interaction
+        self.graph=nx.DiGraph()
+    def forward(self, edge,label,dict_gc):
         time = edge[2]
+        self.edge=edge
+        self.graph.add_nodes_from(edge[:2])
 
-        for i in range(2):  # Two nodes on one side
-            neigh_feat = torch.zeros(self.embedding_dims).unsqueeze(dim=0).to(self.device)
 
-            # Find the neighbor node of the target node at this time
-            neighs_index = torch.nonzero(self.adj[edge[i]] != 0).view(1, -1).squeeze(dim=0)
+        #remove inactive nodes and their edges
+        for node in set(self.graph.nodes()):
+            time_interval = time - self.adj_time[node]
+            if time_interval > self.threshold and node != edge[0] and node != edge[1]:
+                self.graph.remove_node(node)
 
-            #Relative Temporal Encoding
-            temp_node = self.linear_time(torch.abs(torch.Tensor([time-self.adj_time[edge[i]]])).to(self.device))
+        if label == 0:
+            self.graph.add_edge(edge[0], edge[1])
 
-            #Spatial Encoding
-            spa_node = self.linear_node(torch.Tensor([gravity_centrality(self.graph,edge[i])])).to(self.device)
+        for i in range(2):
+            if edge[i] not in self.feature_bank:
+                self.feature_bank[edge[i]]={}
+                self.feature_bank[edge[i]]["feature"]=deque(maxlen=self.window_size)
+                self.feature_bank[edge[i]]["time"] = deque(maxlen=self.window_size)
 
-            #Node Encoding Fusion
-            node_feat =torch.relu(temp_node.unsqueeze(dim=0)+spa_node.unsqueeze(dim=0))
+
+            neigh_feat = torch.zeros(self.embedding_dims).to(self.device).unsqueeze(dim=0)
+            temp_node = self.linear_time(torch.abs(torch.Tensor([time-self.adj_time[edge[i]]])).to(self.device))  # 时间编码
+            eff_tar = self.linear_node(torch.Tensor([dict_gc[int(edge[i])]]).to(self.device))
+            node_feat =eff_tar.unsqueeze(dim=0)+temp_node.unsqueeze(dim=0)
+
+            # node_feat = F.relu(node_feat)
             node_feat = F.normalize(node_feat)
 
-            if len(neighs_index) != 0:
-                for neigh in neighs_index:
-                    neigh_time = self.adj_time[neigh]
-                    if (time - neigh_time) <= self.threshold:
-                        # Relative Temporal Encoding
-                        temp_neigh = self.linear_time(torch.abs(torch.Tensor([time - neigh_time])).to(self.device))
+            in_neighbors=get_in_neighbors(edge[i],self.graph)
+            for neigh in in_neighbors:
+                neigh_time = self.adj_time[neigh]
+                time_score=self.decayer(torch.FloatTensor([time - neigh_time]))
+                temp_neigh = self.linear_time(torch.abs(torch.Tensor([time - neigh_time])).to(self.device))  # 时间编码
+                # temp_neigh = self.time_encode(torch.abs(torch.Tensor([time - neigh_time])).to(self.device))  # 时间编码
+                eff_neigh = self.linear_node(torch.Tensor([dict_gc[int(neigh)]]).to(self.device))
+                neigh_feat_i =eff_neigh.unsqueeze(dim=0)+temp_neigh.unsqueeze(dim=0)
 
-                        # Spatial Encoding
-                        spa_neigh = self.linear_node(torch.Tensor([gravity_centrality(self.graph,neigh)])).to(self.device)
+                # neigh_feat_i = F.relu(neigh_feat_i)
+                neigh_feat_i = F.normalize(neigh_feat_i)
 
-                        # Node Encoding Fusion
-                        neigh_feat_i = torch.relu(temp_neigh.unsqueeze(dim=0)+spa_neigh.unsqueeze(dim=0))
-                        neigh_feat_i = F.normalize(neigh_feat_i)
-
-                        #ATAgg
-                        time_score = self.decayer((time - neigh_time))#Temporal Decay Function
-                        score = self.attention(node_feat.squeeze(dim=0), neigh_feat_i.squeeze(dim=0))#Attention Score
-                        neigh_feat += (time_score*score)*neigh_feat_i
+                attention_score = self.attention(node_feat.squeeze(dim=0), neigh_feat_i.squeeze(dim=0))
+                score =F.leaky_relu(attention_score+time_score)
+                neigh_feat +=(score/len(in_neighbors))*neigh_feat_i
 
             if not self.gcn:
                 combined = torch.cat([node_feat.squeeze(dim=0), neigh_feat.squeeze(dim=0)])
+                # combined = node_feat.squeeze(dim=0)+ neigh_feat.squeeze(dim=0)
             else:
-                combined = neigh_feat.squeeze(dim=0)  # Not aggregate its own node features
+                combined = neigh_feat.squeeze(dim=0)  # 不聚合自身节点特征
+                # self.feature[edge[i]] += combined
+
+            #将队列之前的所有的特征按时间衰减相加，在加上当前的节点特征
+            fearure_i = torch.zeros(self.embedding_dims*2).to(self.device)
+            for index in range(len(self.feature_bank[edge[i]]["feature"])):
+                weight=self.decayer_feature(torch.FloatTensor([time - self.feature_bank[edge[i]]["time"][index]]).to(self.device))
+                fearure_i+=(weight)*self.feature_bank[edge[i]]["feature"][index]
+            fearure_i+=combined
 
             if i == 0:
-                combined_i = combined
+                combined_i = fearure_i
+                self.feat = combined
             else:
-                combined_all = torch.stack((combined_i, combined), dim=0)
+                combined_all = torch.stack((combined_i, fearure_i), dim=0)
+                self.feat = torch.stack((self.feat, combined), dim=0)
 
-        # Update node interaction time
-        for i in range(2):
-            self.adj_time[edge[i]] = time
-
+        self.update_network()
 
         del neigh_feat
-        combined_all=combined_all.mm(self.weight.t())#Linear Layer
+        combined_all=combined_all.mm(self.weight.t())
 
 
-        return combined_all
+        return torch.relu(combined_all)
+        # return combined_all
+
+    def update_network(self):
+        time = self.edge[2]
+        for i in range(2):
+            with torch.no_grad():
+                self.feature_bank[self.edge[i]]["feature"].append(self.feat[i].data)
+                self.feature_bank[self.edge[i]]["time"].append(time)
+                self.adj_time[self.edge[i]] = time
